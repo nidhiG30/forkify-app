fe4b905e3b12b3e33a2a01f4d35eeb4a11578d38
nidhiG30 <gadgenidhi9@gmail.com>
2022-10-04 18:32:13 +0530

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -1460,6 +1460,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function() {
+  console.log('Welcome to the application!');
+}
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -1468,7 +1472,7 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Welcome!');
+  newFeature();
 };
 
 init();
